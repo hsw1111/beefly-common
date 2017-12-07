b4b3578ce0e853e669392f59b7c805149dbf10fa
杨可可 <yangkeke@mmuu.com>
2017-12-07 11:02:41 +0800

    Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import cs from 'classnames';
 
 export default class Button extends React.Component {
 
     render() {
-        let {btnTheme, btnSize, iconClass, onClick, children} = this.props;
-
-        let btnClass = ['btn'];
-        if (btnSize) {
-            btnClass.push('btn-' + btnSize);
-        }
-        if (btnTheme) {
-            btnClass.push('btn-' + btnTheme);
-        }
+        let { theme, size, icon, block, flat, onClick, children } = this.props;
 
         return (
             <div className="form-group">
-                <button type="button" className={btnClass.join(' ')} onClick={onClick}>
-                    {iconClass && <i className={['ace-icon fa bigger-110', 'fa-' + iconClass].join(' ')}></i>}
-                    {children}
+                <button type="button"
+                    className={cs('btn', { [`btn-${theme}`]: theme, [`btn-${size}`]: size, 'btn-block': block, 'btn-flat': flat })} onClick={onClick}>
+                    {icon && <i className={cs('ace-icon', 'fa', 'bigger-110', 'fa-' + icon)}></i>}
+                    &nbsp;{children}&nbsp;
                 </button>
             </div>
         )
@@ -25,17 +19,21 @@ export default class Button extends React.Component {
 
 }
 
-
 Button.propTypes = {
-    btnTheme: React.PropTypes.string,
-    btnSize: React.PropTypes.string,
-    iconClass: React.PropTypes.string,
+    theme: React.PropTypes.oneOf(['', 'default', 'primary', 'success', 'info', 'danger', 'warning',
+        'maroon', 'purple', 'navy', 'orange', 'olive']),
+    size: React.PropTypes.oneOf(['', 'lg', 'md', 'sm', 'xs']),
+    icon: React.PropTypes.string,
+    block: React.PropTypes.bool,
+    flat: React.PropTypes.bool,
     onClick: React.PropTypes.func,
 }
 
 Button.defaultProps = {
-    btnTheme: null,
-    btnSize: null,
-    iconClass: null,
-    onClick: () => null
+    theme: 'primary',   // 主题背影
+    size: '',           // 按钮大小 
+    icon: '',           // 图标名称 
+    block: false,       // 是块元素
+    flat: true,         // 是扁平按钮
+    onClick: () => null,// 单击事件
 }
